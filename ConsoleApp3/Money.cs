a7b6fdf0555d81f8a6aaa4f6326676dc1840e361
1 <1@WANWIN>
2024-11-06 10:14:37 +0500

    Добавьте файлы проекта.

---
+++ ConsoleApp3/Money.cs
@@ -0,0 +1,117 @@
+﻿using System;
+
+internal class Money
+{
+    private uint _rubles;
+    private byte _kopeks;
+
+    public Money(uint rubles, uint kopeks)
+    {
+        _rubles = rubles;
+        _rubles += kopeks / 100;
+        _kopeks = (byte)(kopeks % 100);
+    }
+
+    public void Print()
+    {
+        Console.WriteLine($"{_rubles}Руб. {_kopeks}Коп.");
+    }
+
+    public static bool Srav(Money m1, Money m2)
+    {
+        if (m1._rubles != m2._rubles)
+        {
+            return false;
+        }
+        else if (m1._kopeks != m2._kopeks)
+        {
+            return false;
+        }
+        else
+        {
+            return  true;
+        }
+    }
+
+    // Унарные операции
+    public static Money operator ++(Money m)
+    {
+        m._kopeks++;
+        if (m._kopeks >= 100)
+        {
+            m._kopeks -= 100;
+            m._rubles++;
+        }
+        return m;
+    }
+
+    public static Money operator --(Money m)
+    {
+        if ((m._kopeks == 0) && (m._rubles != 0))
+        {
+            m._kopeks = 99;
+            m._rubles--;
+        }
+        else
+        {
+            if (m._kopeks != 0) m._kopeks--;
+        }
+        return m;
+    }
+
+    //uint
+    public static implicit operator uint(Money m)
+    {
+        return m._rubles;
+    }
+
+    //double
+    public static explicit operator double(Money m)
+    {
+        return m._kopeks / 100.0;
+    }
+
+    // Бинарная операция вычитания копеек
+    public static Money operator -(Money m, int kopeks)
+    {
+        int totalKopeks = (int)(m._rubles * 100) + (int)(m._kopeks);
+
+        totalKopeks -= kopeks;
+
+        if (totalKopeks < 0)
+        {
+            m._rubles = 0;
+            m._kopeks = 0;
+        }
+        else
+        {
+            m._rubles = (uint)(totalKopeks / 100);
+            m._kopeks = (byte)(totalKopeks % 100);
+        }
+        return m;
+    }
+
+    public static Money operator -(int kopeks, Money m)
+    {
+        int totalKopeks = (int)(m._rubles * 100) + (int)(m._kopeks);
+
+        totalKopeks -= kopeks;
+
+        if (totalKopeks < 0)
+        {
+            m._rubles = 0;
+            m._kopeks = 0;
+        }
+        else
+        {
+            m._rubles = (uint)(totalKopeks / 100);
+            m._kopeks = (byte)(totalKopeks % 100);
+        }
+        return m;
+    }
+
+    public override string ToString()
+    {
+        return $"{_rubles}Руб. {_kopeks}Коп.";
+    }
+}
---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+
+namespace ConsoleApp3
+{
+    internal class Program
+    {
+        public static void Main()
+        {
+
+            Console.WriteLine("ВВедите рубли и копейки кошелька 1:");
+            string a_s1 = Console.ReadLine();
+            string a_s2 = Console.ReadLine();
+            if ((uint.TryParse(a_s1, out uint a1)) && (uint.TryParse(a_s2, out uint a2)))
+            {
+                Money a = new Money(a1, a2);
+                Console.WriteLine("ВВедите рубли и копейки кошелька 2:");
+                string b_s1 = Console.ReadLine();
+                string b_s2 = Console.ReadLine();
+                if ((uint.TryParse(b_s1, out uint b1)) && (uint.TryParse(b_s2, out uint b2)))
+                {
+                    Money b = new Money(b1, b2);
+                    Console.WriteLine("ВВедите рубли и копейки кошелька 3:");
+                    string c_s1 = Console.ReadLine();
+                    string c_s2 = Console.ReadLine();
+                    if ((uint.TryParse(c_s1, out uint c1)) && (uint.TryParse(c_s2, out uint c2)))
+                    {
+                        Money c = new Money(c1, c2);
+
+                        Console.WriteLine("Деньги в кошельках:");
+                        a.Print();
+                        b.Print();
+                        c.Print();
+
+                        //Console.WriteLine("\nСравнение:");
+                        //Console.WriteLine("У кого больше? (Денег):");
+                        //Console.WriteLine($"a vs b: {Money.Srav(a, b)}");
+                        //Console.WriteLine($"b vs c: {Money.Srav(b, c)}");
+
+                        // Унарные операции:
+                        Console.WriteLine("\nУнарные операции:");
+                        a++;
+                        Console.Write("a++: ");
+                        a.Print();  //+рубль
+
+                        b--;
+                        Console.Write("b--: ");
+                        b.Print();  //-копейка
+
+                        // Операции приведения типа:
+                        Console.WriteLine("\nперации приведения типа:");
+                        uint rubles = a; //uint
+                        Console.WriteLine($"uint a: {rubles}");
+
+                        double kopeksFraction = (double)b; //double
+                        Console.WriteLine($"double b: {kopeksFraction}");
+
+                        // Бинарные операции:
+                        Console.WriteLine("\nБинарные операции:");
+                        Console.WriteLine("Сколько копеек вычесть из первого кошелька?");
+                        string v_s1 = Console.ReadLine();
+                        if (int.TryParse(v_s1, out int v)){
+                            Money d = a - v;
+                            Console.Write($"a - {v} копеек: ");
+                            d.Print();  //вычет копеек
+                        }
+                        else
+                        {
+                            Console.WriteLine("Ошибка ввода:");
+                        }
+
+                    }
+                    else
+                    {
+                        Console.WriteLine("Ошибка ввода:");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Ошибка ввода:");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Ошибка ввода:");
+            }
+
+            
+
+            
+
+
+            
+
+        }
+    }
+}
